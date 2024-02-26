5ed04790411538f1fbe9c171461688d073a19d7e
Ali <amammadov097@gmail.com>
2024-02-26 20:20:56 +0100

    made 4 models and saved them

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    quantity = models.FloatField(default=0)
+    unit = models.CharField(max_length=200)
+    price_per_unit = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return f'{self.name} {self.unit} {self.price_per_unit}'
+    
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.IntegerField(default=0)
+
+
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField(default=0)
+
+
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
